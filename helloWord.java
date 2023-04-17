38f619b263b966bc9adf48aac9e76ebbc8c35f47
leelxr <122518345+leelxr@users.noreply.github.com>
2023-04-17 20:16:00 +0800

    Create helloWord.java

---
+++ helloWord.java
@@ -0,0 +1,19 @@
+/**
+ * Hello world
+ *
+ */
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    String s = null;
+    System.out.println(s.length());
+    s = "傻逼";
+  }
+      @Override
+    public String toString() {
+        return null;
+    }
+
+}
